392614461ebef5ff52faf82ca5263e20e17fcc52
Joseph Elliott <joeboyscout04@gmail.com>
2019-09-24 21:01:33 +0200

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "IGListKit",
+    platforms: [.iOS(.v8)],
     products: [
         .library(name: "IGListKit", targets: ["IGListKit"])
     ],
     targets: [
         .target(
             name: "IGListKit",
-            path: "Source",
-            sources: ["Common", "Common/Internal", "Internal"],
-            publicHeadersPath: "."
-        )
+            dependencies: [])
     ]
 )
