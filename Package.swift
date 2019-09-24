bf3fc6d06f78a44926d0dd1b50ddfb1e81f802ec
Petteri Hyttinen <petteri.hyttinen@gmail.com>
2019-09-24 13:47:57 +0200

    tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
@@ -10,8 +10,7 @@ let package = Package(
         .target(
             name: "IGListKit",
             path: "Source",
-            sources: ["Common", "Common/Internal", "Internal"],
-            publicHeadersPath: "."
+            sources: ["Common", "Common/Internal", "Internal"]
         )
     ]
 )
